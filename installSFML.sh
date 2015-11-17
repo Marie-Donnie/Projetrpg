6b4fad07733630a413e4dd5f56c0f28512f2015c
Marie-Donnie <aldanna.riel@gmail.com>
2015-11-17 22:36:40 +0100

    Ca a intérêt à marcher, là

--- installSFML.sh
+++ installSFML.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 sudo apt-get install libboost-dev
 wget http://www.sfml-dev.org/files/SFML-2.3.2-sources.zip
 unzip SFML-2.3.2-sources.zip
@@ -5,6 +6,15 @@ mv SFML-2.3.2 SFML
 rm SFML-2.3.2-sources.zip
 cd SFML
 sudo apt-get build-dep libsfml
+sudo apt-get install libxcb1-dev
+sudo apt-get install libx11-xcb-dev
+sudo apt-get install libxcb-randr0-dev
+sudo apt-get install libxcb-image0
+sudo apt-get install libflac-dev
+sudo apt-get install libogg-dev
+sudo apt-get install libvorbis-dev
+sudo apt-get install libvorbisenc2
+sudo apt-get install libvorbisfile3
 mkdir build
 cd build
 cmake .. -G "Unix Makefiles"
